1f8a30c03443197aab6221785167673988d2da46
codewithalihamza <512syedalihamza@gmail.com>
2024-05-19 13:52:18 +0500

    Design Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,29 @@
+'use client';
+import { cookies } from '@/config/cookies-config';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    cookies.remove('token');
+    router.push('/login');
+  };
+
   return (
-    <div>
-      MiddleWare
+    <div className="container-home">
+      {/* Header */}
+      <header className="header-home">
+        <div className="headerContent-home">
+          <h1 className="title-home">MiddleWare</h1>
+          <button onClick={handleLogout} className="logoutButton-home">Logout</button>
+        </div>
+      </header>
+
+      {/* Main Content */}
+      <div className="mainContent-home">
+        Do You Know About Middleware? Comment Please
+      </div>
     </div>
   );
 }
