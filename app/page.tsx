d45e9fae33e6e39d97c6e9516d0af01ec9ea2c32
codewithalihamza <512syedalihamza@gmail.com>
2024-05-19 13:58:56 +0500

    design home page main content

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,23 @@ export default function Home() {
 
       {/* Main Content */}
       <div className="mainContent-home">
-        Do You Know About Middleware? Comment Please
+        <h2>Let's Watch</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod felis id lectus sollicitudin, a tincidunt sapien blandit. Fusce laoreet, libero a tincidunt mattis, felis mi iaculis ipsum, vitae fermentum quam neque in odio.
+        </p>
+        {/* Embedded YouTube Video */}
+        <div className="videoWrapper">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/jjtFxzyUMSE?si=esSuDlkVxbM-wbpD"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
     </div>
   );
